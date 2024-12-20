583cebf333858d94fd6dfcd859a77a98b7424ddf
Jason Dai <djdjz7@icloud.com>
2024-12-21 01:21:30 +0800

    fix: build

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from '@/router/router';
 import { shallowRef, watch } from 'vue';
-const route = useRoute();
+const route = useRoute(() => null);
 const component = shallowRef(route.mainComponent);
 
 watch(route, (newVal) => {
