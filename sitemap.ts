c26b88e28e2e658bb1f76b82d67817c08cf01023
djdjz7 <djdjz7@icloud.com>
2025-02-09 10:48:20 +0800

    feat: make sitemap generator more generic

--- sitemap.ts
+++ sitemap.ts
@@ -5,62 +5,35 @@ import matter from 'gray-matter'
 import path from 'path'
 import fs from 'fs'
 
-const activities = fg
-  .sync('./src/data/activities/*.md')
-  .map((entry) => {
-    return { entry, frontmatter: matter.read(entry) }
-  })
-  .map((file) => {
-    const { entry, frontmatter } = file
-    if (frontmatter.data.time === undefined) return
-    const filename = path.parse(entry).name
-    return {
-      url: `/activities/${filename}.html`,
-      lastmod: frontmatter.data.time,
-      changefreq: 'monthly',
-      priority: 0.5,
-    }
-  })
-  .filter((file) => file !== undefined)
-  .sort((a, b) => new Date(b.lastmod).getTime() - new Date(a.lastmod).getTime())
-
-const news = fg
-  .sync('./src/data/news/*.md')
-  .map((entry) => {
-    return { entry, frontmatter: matter.read(entry) }
-  })
-  .map((file) => {
-    const { entry, frontmatter } = file
-    const filename = path.parse(entry).name
-    return {
-      url: `/news/${filename}.html`,
-      lastmod: frontmatter.data.time,
-      changefreq: 'monthly',
-      priority: 0.5,
-    }
-  })
-  .sort((a, b) => new Date(b.lastmod).getTime() - new Date(a.lastmod).getTime())
-
-const links = [
-  { url: '/', changefreq: 'daily', priority: 1, lastmod: new Date().toISOString() },
-  {
-    url: '/activities/',
+const categories = ['activities', 'news', 'announcements']
+const links = []
+for (const category of categories) {
+  const pages = fg
+    .sync(`./src/data/${category}/*.md`)
+    .map((entry) => {
+      return { entry, frontmatter: matter.read(entry) }
+    })
+    .map((file) => {
+      const { entry, frontmatter } = file
+      const filename = path.parse(entry).name
+      return {
+        url: `/${category}/${filename}.html`,
+        lastmod: frontmatter.data.time,
+        changefreq: 'monthly',
+        priority: 0.5,
+      }
+    })
+    .sort((a, b) => new Date(b.lastmod).getTime() - new Date(a.lastmod).getTime())
+  links.push(...pages)
+  links.push({
+    url: `/${category}/`,
     changefreq: 'daily',
     priority: 0.8,
-    lastmod:
-      activities.length > 0
-        ? new Date(activities[0].lastmod).toISOString()
-        : new Date().toISOString(),
-  },
-  {
-    url: '/news/',
-    changefreq: 'daily',
-    priority: 0.8,
-    lastmod: news.length > 0 ? new Date(news[0].lastmod).toISOString() : new Date().toISOString(),
-  },
-]
+    lastmod: pages.length > 0 ? new Date(pages[0].lastmod).toISOString() : new Date().toISOString(),
+  })
+}
 
-links.push(...activities, ...news)
+links.push({ url: '/', changefreq: 'daily', priority: 0.8, lastmod: new Date().toISOString() })
 
 const stream = new SitemapStream({ hostname: 'https://lcpu.dev' })
 const buffer = await streamToPromise(Readable.from(links).pipe(stream))
