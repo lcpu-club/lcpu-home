7e3f7a8cff78a3cc776c0c172365b4aaca6a5755
Holger Huo <50446405+HolgerHuo@users.noreply.github.com>
2025-02-09 22:37:41 +0800

    update: make list title clickable

--- src/components/PageListEntry.vue
+++ src/components/PageListEntry.vue
@@ -9,7 +9,7 @@ defineProps<{
 
 <template>
   <div border-t="1 gray-200 dark:dark-100 solid" class="news-list-entry" p-y-4 box-border>
-    <h3 m-0>{{ pageEntry.title }}</h3>
+    <a :href="pageEntry.contentUrl" text-inherit no-underline><h3 m-0>{{ pageEntry.title }}</h3></a>
     <div flex="~ items-center gap-1" m-t-1 text-gray-500 dark:text-truegray-400>
       <span>{{ dateString(pageEntry.time) }}</span>
       <span flex="~ gap-1" v-for="key in Object.keys(pageEntry.data)" :key="key">
