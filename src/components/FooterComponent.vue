fa2c5c1ab26e30e7011d3a81a928e7f0a1bbab00
HolgerHuo <50446405+HolgerHuo@users.noreply.github.com>
2025-02-12 23:19:25 +0800

    fix(typo): Github to GitHub

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,7 +6,7 @@ const year = new Date().getFullYear()
   <footer text-center flex="~ col items-center" text-sm text-gray-400 dark:text-truegray-600>
     <div flex="~ items-center gap-2">
       <a text-unset href="/about/">关于我们</a>
-      <a text-unset href="https://github.com/lcpu-club" target="_blank">Github</a>
+      <a text-unset href="https://github.com/lcpu-club" target="_blank">GitHub</a>
       <a text-unset href="https://space.bilibili.com/3461562830424779" target="_blank">哔哩哔哩</a>
       <a text-unset href="mailto:linuxclub@pku.edu.cn">邮件联系</a>
     </div>
